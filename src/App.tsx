9ce0eef4d662df95608084903ef8b777fb39abb6
Alisher Battalov <71417582+abattalov@users.noreply.github.com>
2025-09-06 15:59:56 -0500

    added green background

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Card from "./components/Card";
+import "./App.css";
 import type { GameState } from "./types/types";
 import { createStandardDeck, dealCards, getNextCard, getTieCards, shuffleDeck } from "./utils/deckUtils";
 import { useEffect, useState } from 'react';
@@ -88,57 +89,57 @@ function App() {
     }
   }
 
-  // return (
-  //   <div style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
-  //     <div style={{ display: "flex", gap: "10px" }}>
-  //       <p>Player 1: {gameState.player1.length}</p>
-  //       <p>Player 2: {gameState.player2.length}</p>
-  //     </div>
-
-  //     <div style={{ display: "flex", gap: "10px" }}>
-  //       <button onClick={() => setAutoPlay(!autoPlay)}>
-  //         {autoPlay ? "Stop Auto Play" : "Start Auto Play"}
-  //       </button>
-  //       <button onClick={handleNextHand}>next hand</button>
-  //       <button onClick={resolveRound}>resolve round</button>
-  //     </div>
-
-  //     {gameState.game && gameState.game.player1Card && gameState.game.player2Card && (
-  //       <div>
-  //         {gameState.game.player1Card.length > 1 && <p style={{ color: 'red', fontWeight: 'bold' }}>WAR!</p>}
-  //         <div style={{ display: "flex", gap: "10px" }}>
-  //           <div>
-  //             <p>Player 1 ({gameState.game.player1Card.length} cards)</p>
-  //             <Card card={gameState.game.player1Card[gameState.game.player1Card.length - 1]} />
-  //           </div>
-  //           <div>
-  //             <p>Player 2 ({gameState.game.player2Card.length} cards)</p>
-  //             <Card card={gameState.game.player2Card[gameState.game.player2Card.length - 1]} />
-  //           </div>
-  //         </div>
-  //       </div>
-  //     )}
-  //   </div>
-  // );
-
-
   return (
-    <div style={{ display: "flex", gap: "10px" }}>
-      <p>Player 1: {gameState.player1.length}</p>
-      <p>Player 2: {gameState.player2.length}</p>
-      <button onClick={() => setAutoPlay(!autoPlay)}>
-        {autoPlay ? "Stop Auto Play" : "Start Auto Play"}
-      </button>
-      <button onClick={handleNextHand}>next hand</button>
-      <button onClick={resolveRound}>resolve round</button>
-      {gameState.game && gameState.game.player1Card && gameState.game.player2Card &&
-        <>
-          <Card card={gameState.game.player1Card[0]}/>
-          <Card card={gameState.game.player2Card[0]}/>
-        </>
-      }
+    <div style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <p>Player 1: {gameState.player1.length}</p>
+        <p>Player 2: {gameState.player2.length}</p>
+      </div>
+
+      <div style={{ display: "flex", gap: "10px" }}>
+        <button onClick={() => setAutoPlay(!autoPlay)}>
+          {autoPlay ? "Stop Auto Play" : "Start Auto Play"}
+        </button>
+        <button onClick={handleNextHand}>next hand</button>
+        <button onClick={resolveRound}>resolve round</button>
+      </div>
+
+      {gameState.game && gameState.game.player1Card && gameState.game.player2Card && (
+        <div>
+          {gameState.game.player1Card.length > 1 && <p style={{ color: 'red', fontWeight: 'bold' }}>WAR!</p>}
+          <div style={{ display: "flex", gap: "10px" }}>
+            <div>
+              <p>Player 1 ({gameState.game.player1Card.length} cards)</p>
+              <Card card={gameState.game.player1Card[gameState.game.player1Card.length - 1]} />
+            </div>
+            <div>
+              <p>Player 2 ({gameState.game.player2Card.length} cards)</p>
+              <Card card={gameState.game.player2Card[gameState.game.player2Card.length - 1]} />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
+
+
+  // return (
+  //   <div style={{ display: "flex", gap: "10px" }}>
+  //     <p>Player 1: {gameState.player1.length}</p>
+  //     <p>Player 2: {gameState.player2.length}</p>
+  //     <button onClick={() => setAutoPlay(!autoPlay)}>
+  //       {autoPlay ? "Stop Auto Play" : "Start Auto Play"}
+  //     </button>
+  //     <button onClick={handleNextHand}>next hand</button>
+  //     <button onClick={resolveRound}>resolve round</button>
+  //     {gameState.game && gameState.game.player1Card && gameState.game.player2Card &&
+  //       <>
+  //         <Card card={gameState.game.player1Card[0]}/>
+  //         <Card card={gameState.game.player2Card[0]}/>
+  //       </>
+  //     }
+  //   </div>
+  // );
 }
 
 export default App;
